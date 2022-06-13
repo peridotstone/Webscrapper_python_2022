11d30cc15620257de07a0d4beabe681b74d8b1ed
peridotstone <peridotstone@naver.com>
2022-06-14 06:53:48 +0900

    study function argument
    positional
    keyword
    f-string
    formatting ,,,

---
+++ README.md
@@ -0,0 +1,1 @@
+python study site : https://docs.python.org/3.9/library/
---
+++ Test.py
@@ -0,0 +1,6 @@
+
+def plus(first, second):
+    if(first == NULL or second == NULL):
+        return "input argument"
+    else:
+        return {} + {}.__format__(first, second)
