01d5cd8c48fc49645c69330a2393fa17801bee0a
peridotstone <107038501+peridotstone@users.noreply.github.com>
2022-06-14 21:13:04 +0900

    study calculator
    and f string

--- Test.py
+++ Test.py
@@ -1,6 +1,28 @@
+def plus(a,b):
+    return calculator(a,b,"+")
 
-def plus(first, second):
-    if(first == NULL or second == NULL):
-        return "input argument"
+def negation(a):
+    if a.isdecimal():
+        a=int(a)
+        return a*-1
     else:
-        return {} + {}.__format__(first, second)
+        return "please enter a number"
+
+
+def calculator(a,b,math):
+    try:
+        a=int(a)
+        b=int(b)
+        if math == "+":
+            return a+b
+        
+    except:
+        return "please enter a number"
+    
+print(plus(10,10))
+print(negation("law"))
+print(negation("10"))
+
+
+a = chr(54)
+print(f'1. chr(54) = {a}, type(a) = {type(a)}')
