a8cd68ab2a055d1967945870f42d53c4ac610f12
peridotstone <peridotstone@naver.com>
2022-06-15 01:28:01 +0900

    assignment2

---
+++ assignment2.py
@@ -0,0 +1,47 @@
+"""
+As you can see, the code is broken.
+Create the missing functions, use default arguments.
+Sometimes you have to use 'return' and sometimes you dont.
+Start by creating the functions
+"""
+
+
+def is_on_list(list_days, str_day):
+    if str_day in list_days:
+        return True
+    else:
+        return "Nothing in days"
+
+
+def get_x(list_days, int_ord):
+    return list_days[int_ord]
+
+
+def add_x(list_days, str_day):
+    list_days += [str_day]
+
+
+def remove_x(list_days, str_day):
+    list_days.remove(str_day)
+
+
+# \/\/\/\/\/\/\  DO NOT TOUCH AREA  \/\/\/\/\/\/\ #
+
+days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+
+
+print("Is Wed on 'days' list?", is_on_list(days, "Wed"))
+
+
+print("The fourth item in 'days' is:", get_x(days, 3))
+
+
+add_x(days, "Sat")
+print(days)
+
+
+remove_x(days, "Mon")
+print(days)
+
+
+# /\/\/\/\/\/\/\ END DO NOT TOUCH AREA /\/\/\/\/\/\/\ #
