2f2fc0de5da4f34e34d5f1179d236fe859c7f9fc
peridotstone <107038501+peridotstone@users.noreply.github.com>
2022-06-20 06:02:07 +0900

    Update indeed&stackoverflow_scrapper.py

--- indeed&stackoverflow_scrapper.py
+++ indeed&stackoverflow_scrapper.py
@@ -1,5 +1,20 @@
 import requests
-import bs4
+import warnings
+from bs4 import BeautifulSoup
+
+warnings.filterwarnings("ignore")
 
 indeed_result = requests.get(
-    "https://www.indeed.com/jobs?q=python&limit=50", verify=False)
+    "https://www.indeed.com/jobs?q=python&limit=50", verify=False, timeout=3)
+
+indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
+
+pagination = indeed_soup.find("div", {"class":"pagination"})
+
+pages = pagination.find_all('a')
+spans=[]
+
+for page in pages:
+    spans.append(page.find("span"))
+    
+print(spans[:-1])
